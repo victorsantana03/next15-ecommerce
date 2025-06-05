b44e6dd753120982664c3918116b1ecd0eb4e7ab
victorsantana03 <viclucigei.123@gmail.com>
2025-06-04 23:03:41 -0300

    Iniciando config de autenticação do Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-slate-700 antialiased`}
-      >
-        <Navbar />
-        <main className="h-screen p-16">{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} bg-slate-700 antialiased`}
+        >
+          <Navbar />
+          <main className="h-screen p-16">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
